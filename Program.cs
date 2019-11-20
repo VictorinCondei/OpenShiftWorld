fd7fc99f9ff27575fc673f212d8f3b14a5d53c21
Victorin Condei <victorin_condei@yahoo.com>
2019-11-20 10:51:49 +0200

    update 00

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,11 +15,20 @@ namespace OpenShiftWorld
     {
         public static void Main(string[] args)
         {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            Directory.SetCurrentDirectory(pathToContentRoot);
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureKestrel((context, options) => { options.Configure(context.Configuration.GetSection("Kestrel")); })
                 .UseStartup<Startup>();
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
     }
 }
