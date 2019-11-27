8b875b6477e2e985f75fd4ed878d14d7947ff674
Victorin Condei <victorin_condei@yahoo.com>
2019-11-27 14:04:59 +0200

    add infos

---
+++ Controllers/InfosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenShiftWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfosController : ControllerBase
+    {
+        [HttpGet("openshift")]
+        public string GetOS()
+        {
+            return "Hello To OpenShift";
+        }
+        // GET: api/Infos
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Infos/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Infos
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Infos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
